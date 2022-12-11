923ad180bc34efd015a4352479837440eb835bf4
Rujan666 <116330005+Rujan666@users.noreply.github.com>
2022-12-11 23:08:09 +0545

    Laravel project

---
+++ myProjectt/resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia cumque necessitatibus architecto eum dolores reiciendis, doloribus in magni quos veritatis illo magnam? Earum deserunt minus perferendis dolor fugit molestias fugiat!</h1>
+    
+</body>
+</html>
