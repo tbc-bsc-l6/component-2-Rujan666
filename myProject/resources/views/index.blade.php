79a608af6c0ba3173a14ba3202f0fd4083c43a66
Rujan666 <116330005+Rujan666@users.noreply.github.com>
2023-01-18 14:53:24 +0545

    New home page add and deleted the last proejct Please enter the commit message for your changes. Lines starting

--- myProject/resources/views/index.blade.php
+++ myProject/resources/views/index.blade.php
@@ -52,8 +52,15 @@
   </div>
 </nav>
 </div>
-    
+<body>
+  <div class="container">
+    <img class ="w-100"src="../img/laravel2.jpg" alt="no image found">
+  </div>   
 </body>
+<!-- Footer-->
+<footer class="bg-light py-5">
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2022 - Lucifer MorningStar</div></div>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </html>
